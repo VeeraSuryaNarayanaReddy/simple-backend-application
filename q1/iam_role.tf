fb6303eb3efdcb5795623c0d812b515d7dc2cfd1
VeeraSuryaNarayanaReddy <rnarayana183@gmail.com>
2025-04-26 19:14:12 +0530

    first committ

---
+++ q1/iam_role.tf
@@ -0,0 +1,65 @@
+resource "aws_iam_role" "ec2_role" {
+  name = "ec2-s3-access-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        },
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_policy" "s3_full_access_policy" {
+  name        = "S3FullAccessCustomPolicy"
+  description = "Allow full access to S3 buckets"
+  policy      = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect   = "Allow",
+        Action   = "s3:*",
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_policy" "ec2_control_policy" {
+  name        = "EC2ControlCustomPolicy"
+  description = "Allow starting, stopping, describing EC2 instances"
+  policy      = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect   = "Allow",
+        Action   = [
+          "ec2:StartInstances",
+          "ec2:StopInstances",
+          "ec2:DescribeInstances"
+        ],
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "attach_s3_policy" {
+  role       = aws_iam_role.ec2_role.name
+  policy_arn = aws_iam_policy.s3_full_access_policy.arn
+}
+
+resource "aws_iam_role_policy_attachment" "attach_ec2_policy" {
+  role       = aws_iam_role.ec2_role.name
+  policy_arn = aws_iam_policy.ec2_control_policy.arn
+}
+
+resource "aws_iam_instance_profile" "ec2_instance_profile" {
+  name = "ec2-instance-profile"
+  role = aws_iam_role.ec2_role.name
+}
